c55fb66c370550cb153e4138f1e7e3eff3f87425
jiangwei <1476540891@qq.com>
2024-05-13 21:48:30 +0800

    创建试图login

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
-qfrom djiangi.http import HttpResponse
-
+from djiangi.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(requesr):
 	return HttpResponse("index")
+
+def login(request):
+	return redirect("/index')
